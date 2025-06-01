4f402f242430feb1f26c3d4d5d60430e53560b6e
Daniel Wuensche <daniel@dvoid.xyz>
2025-06-01 18:30:49 +0200

    fix syntax

--- global/run/user.sh
+++ global/run/user.sh
@@ -5,14 +5,14 @@ SETUP_DIR=${SETUP_DIR:-$HOME/linux-setup}
 source_file="${SETUP_DIR}/global/data/home/daniel/.aliases"
 cp $source_file /home/daniel/.aliases
 if [[ -f "${source_file}-local_$(hostname)" ]]; then
-    source_file="${source_file}-local_$(hostname)
+    source_file="${source_file}-local_$(hostname)"
     cp $source_file /home/daniel/.aliases-local_$(hostname)
 fi
 
 source_file="${SETUP_DIR}/global/data/home/daniel/.profile"
 cp $source_file /home/daniel/.profile
 if [[ -f "${source_file}-local_$(hostname)" ]]; then
-    source_file="${source_file}-local_$(hostname)
+    source_file="${source_file}-local_$(hostname)"
     cp $source_file /home/daniel/.profile-local_$(hostname)
 fi
 
