51721e91ebcf4fb9427165f24efe0e744eca55b3
Daniel Wuensche <daniel@dvoid.xyz>
2025-07-06 20:33:27 +0200

    fix nvidia.sh, add igpu mode

--- local_daniel-fusion15/run/nvidia.sh
+++ local_daniel-fusion15/run/nvidia.sh
@@ -3,8 +3,8 @@
 SETUP_DIR=${SETUP_DIR:-$HOME/linux-setup}
 
 # find current linux kernel
-current_linux=pamac list -iq | grep -P 'linux\d+$'
-sudo pamac install ${current_linux}-nvidia-open
+current_linux=$(pamac list -iq | grep -P 'linux\d+$')
+sudo pamac install "${current_linux}-nvidia-open"
 
 # nvidia module options
 source_file="${SETUP_DIR}/local_$(hostname)/data/etc/modprobe.d/nvidia-pm.conf"
@@ -25,8 +25,19 @@ sudo cp $source_file /usr/local/bin/plasmawayland-dgpu
 sudo chown root:root /usr/local/bin/plasmawayland-dgpu
 sudo chmod 0755 /usr/local/bin/plasmawayland-dgpu
 
+# wayland session for iGPU-only mode
+source_file="${SETUP_DIR}/local_$(hostname)/data/usr/share/wayland-sessions/plasmawayland-igpu.desktop"
+sudo cp $source_file /usr/share/wayland-sessions/plasmawayland-igpu.desktop
+sudo chown root:root /usr/share/wayland-sessions/plasmawayland-igpu.desktop
+sudo chmod 0644 /usr/share/wayland-sessions/plasmawayland-igpu.desktop
+
+source_file="${SETUP_DIR}/local_$(hostname)/data/usr/local/bin/plasmawayland-igpu"
+sudo cp $source_file /usr/local/bin/plasmawayland-igpu
+sudo chown root:root /usr/local/bin/plasmawayland-igpu
+sudo chmod 0755 /usr/local/bin/plasmawayland-igpu
+
 # scripts to disable/enable dgpu
-sudo pamac install ${current_linux}-acpi_call
+sudo pamac install "${current_linux}-acpi_call"
 
 source_file="${SETUP_DIR}/local_$(hostname)/data/usr/local/bin/gpuoff"
 sudo cp $source_file /usr/local/bin/gpuoff
