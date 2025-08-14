a4a927a01db26a86ce31397bb9ec36fcbd7dea4d
Daniel Wuensche <daniel@dvoid.xyz>
2025-08-14 18:52:16 +0200

    add host daniel-pc-manjaro

---
+++ vars/daniel-pc-manjaro/roles.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+roles=(
+    "system"
+    "grub"
+    "sddm"
+    "sudoers"
+    "thermald"
+    "dotfiles"
+    "neovim"
+    "brave-browser"
+    "thunderbird"
+    "discord"
+)
